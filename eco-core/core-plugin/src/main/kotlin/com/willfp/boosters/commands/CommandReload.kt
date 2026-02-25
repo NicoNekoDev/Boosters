f74882b8810ec8dee914127526e4f7040c5485bc
NicoNeko <nicuch100@gmail.com>
2026-02-25 20:10:04 +0200

    Add time to reload

--- eco-core/core-plugin/src/main/kotlin/com/willfp/boosters/commands/CommandReload.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/boosters/commands/CommandReload.kt
@@ -3,6 +3,8 @@ package com.willfp.boosters.commands
 import com.willfp.boosters.plugin
 import com.willfp.eco.core.Prerequisite
 import com.willfp.eco.core.command.impl.Subcommand
+import com.willfp.eco.util.StringUtils
+import com.willfp.eco.util.toNiceString
 import org.bukkit.command.CommandSender
 
 object CommandReload : Subcommand(
@@ -14,8 +16,10 @@ object CommandReload : Subcommand(
 
     override fun onExecute(sender: CommandSender, args: List<String>) {
         val runnable: Runnable = {
-            plugin.reload()
-            sender.sendMessage(plugin.langYml.getMessage("reloaded"))
+            sender.sendMessage(
+                plugin.langYml.getMessage("reloaded", StringUtils.FormatOption.WITHOUT_PLACEHOLDERS)
+                    .replace("%time%", plugin.reloadWithTime().toNiceString())
+            )
         }
         if (Prerequisite.HAS_FOLIA.isMet)
             plugin.scheduler.runTask(runnable) // run on global thread
