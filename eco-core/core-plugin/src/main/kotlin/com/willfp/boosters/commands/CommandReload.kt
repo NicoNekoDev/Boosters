7d0930203fe08bf7fa5134b55581599eb5b17726
NicoNeko <nicuch100@gmail.com>
2026-02-25 19:35:00 +0200

    Reload on main/global thread

--- eco-core/core-plugin/src/main/kotlin/com/willfp/boosters/commands/CommandReload.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/boosters/commands/CommandReload.kt
@@ -1,6 +1,7 @@
 package com.willfp.boosters.commands
 
 import com.willfp.boosters.plugin
+import com.willfp.eco.core.Prerequisite
 import com.willfp.eco.core.command.impl.Subcommand
 import org.bukkit.command.CommandSender
 
@@ -12,7 +13,13 @@ object CommandReload : Subcommand(
 ) {
 
     override fun onExecute(sender: CommandSender, args: List<String>) {
-        plugin.reload()
-        sender.sendMessage(plugin.langYml.getMessage("reloaded"))
+        val runnable: Runnable = {
+            plugin.reload()
+            sender.sendMessage(plugin.langYml.getMessage("reloaded"))
+        }
+        if (Prerequisite.HAS_FOLIA.isMet)
+            plugin.scheduler.runTask(runnable) // run on global thread
+        else
+            runnable.run()
     }
 }
